b0f18cee0c515345696af05b249d91076c739fef
douglasfreire <douglasfreire20@gmail.com>
2020-05-17 12:59:11 -0300

    Sequencia de Fibonacci infinita

---
+++ estrutura_controle_projetos/fibonacci_v1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+'''
+Sequencia de Fibonacci  infinita
+'''
+def fibonacci():
+    penultimo = 0
+    ultimo = 1
+    print(f'{penultimo}, {ultimo}', end=', ')
+
+    while True:
+        proximo = penultimo + ultimo
+        print(proximo, end=', ')
+        penultimo = ultimo
+        ultimo = proximo
+
+
+if __name__ == '__main__':
+    fibonacci()
